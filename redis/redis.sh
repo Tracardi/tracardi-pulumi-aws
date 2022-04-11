0735defeaa0f6a908d854b954e2e84802afbf112
risto <risto.kowaczewski@gmail.com>
2022-04-11 10:17:05 +0200

    Redis pods

---
+++ redis/redis.sh
@@ -0,0 +1,16 @@
+# this setup is based on https://www.containiq.com/post/deploy-redis-cluster-on-kubernetes
+
+kubectl create ns redis
+kubectl apply -f redis-storage.yaml
+kubectl apply -n redis -f redis-config.yaml
+kubectl apply -n redis -f redis-statefulset.yaml
+
+# The service endpoints for each Redis pod are given below:
+# syntax
+# pod_name.service_name.namespace.svc.cluster.local
+# 
+# Example
+# redis-0.redis.redis.svc.cluster.local
+# redis-1.redis.redis.svc.cluster.local
+# redis-2.redis.redis.svc.cluster.local
+
