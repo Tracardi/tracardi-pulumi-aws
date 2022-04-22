f55862cced752679ed02203425cf1cfa1fe8c552
risto <risto.kowaczewski@gmail.com>
2022-04-22 23:00:49 +0200

    Elastic snapshot

--- elastic/snapshot.sh
+++ elastic/snapshot.sh
@@ -1,3 +1,5 @@
 curl -X PUT "localhost:9200/_snapshot/tracardi_backup?pretty" -H 'Content-Type: application/json' -d' { "type": "fs", "settings": { "location": "/usr/share/elasticsearch/data/snapshot", "compress": true } }'
 curl -X PUT "localhost:9200/_snapshot/tracardi_backup/snapshot-0?pretty"
 curl -X GET "localhost:9200/_snapshot/tracardi_backup/snapshot-0?pretty"
+curl -X DELETE "localhost:9200/_snapshot/tracardi_backup/snapshot-0?pretty"
+curl -X GET "localhost:9200/_snapshot/tracardi_backup/_all?pretty"
