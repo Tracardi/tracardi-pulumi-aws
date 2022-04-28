df86e1224dc0419639645d1e57972179b1e52ce0
risto <risto.kowaczewski@gmail.com>
2022-04-29 00:36:07 +0200

    k8s - digital ocean

--- tracardi/tracardi.sh
+++ tracardi/tracardi.sh
@@ -2,3 +2,4 @@ kubectl create ns tracardi
 kubectl apply -f tracardi-web-api.yaml -n tracardi
 kubectl apply -f tracardi-gui-api.yaml -n tracardi
 kubectl apply -f tracardi-gui.yaml -n tracardi
+kubectl apply -f tracardi-ingress.yaml -n tracardi
